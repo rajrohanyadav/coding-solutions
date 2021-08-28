dc71d0533f7b57ae8ba5b817619227d21095d34e
Azad Nautiyal <azadnautiyal54.an@gmail.com>
2021-08-28 22:01:38 +0530

    Add function to return indices of pair sum.

--- solutions/two-sum/TwoSum.java
+++ solutions/two-sum/TwoSum.java
@@ -12,11 +12,24 @@ public class TwoSum
             int targetSum = s.nextInt();
             s.nextLine();
             int[] nums = Arrays.stream(s.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
-            System.out.println(Arrays.toString(twoSum(nums, targetSum)));
+            System.out.println(Arrays.toString(twoSumIndices(nums, targetSum)));
         }
     }
+    
+    public static int[] twoSumIndices(int[] nums, int targetSum) // Returns the indices of pair giving target sum.
+    {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            int potentialMatch = targetSum - nums[i];
+            if (map.containsKey(potentialMatch))
+                return new int[]{i, map.get(potentialMatch)};
+            else
+                map.put(nums[i], i);
+        }
+        return new int[]{}; // No match found
+    }
 
-    public static int[] twoSum(int[] nums, int targetSum)
+    public static int[] twoSumNums(int[] nums, int targetSum) // Returns the actual pair giving target sum.
     {
         HashMap<Integer, Boolean> map = new HashMap<>();
         for (int num : nums) {
