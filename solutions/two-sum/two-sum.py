c6aefae52933941c69853895072c3e57b4a03cb7
Rohan Yadav <rajrohanyadav@gmail.com>
2021-12-12 15:22:32 +0530

    add py solution to two-sum problem

--- solutions/two-sum/two-sum.py
+++ solutions/two-sum/two-sum.py
@@ -1,1 +1,12 @@
 # Python solution to the two-sum problem
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        required_sum = target
+        for enum, element in enumerate(nums):
+            required_sum = target - element
+            if required_sum in hash_map.keys():
+                return (enum, hash_map[required_sum])
+            else:
+                hash_map[element] = enum
+        
